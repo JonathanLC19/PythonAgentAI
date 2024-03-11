43cc847a624ea9738d802261270395e0bac92939
Jonathan López <102967052+JonathanLC19@users.noreply.github.com>
2024-03-11 08:49:56 +0000

    update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ population_path = os.path.join("data", "population.csv")
 population_df = pd.read_csv(population_path)
 
 population_query_engine = PandasQueryEngine(
-    df=population_df, verbose=True, instruction_str=instruction_str
+    df=population_df, verbose=False, instruction_str=instruction_str
 )
 population_query_engine.update_prompts({"pandas_prompt": new_prompt})
 population_query_engine.query("what is the population of canada?")
